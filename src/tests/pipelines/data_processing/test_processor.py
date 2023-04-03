4ca66490b891878ecfccb5dead8043e4ecfcf5e7
ARED2000 <arthur.decaluwe@be.ey.com>
2023-04-03 18:12:02 +0200

    Some tests added

--- src/tests/pipelines/data_processing/test_processor.py
+++ src/tests/pipelines/data_processing/test_processor.py
@@ -2,8 +2,8 @@ import pandas as pd
 from pandas.testing import assert_frame_equal, assert_series_equal
 
 from eyft.pipelines.data_processing.processor import (
-    mean_impute, mode_impute, z_normalise, min_max_scale,
-    cap_3std
+    mean_impute, mode_impute, median_impute, z_normalise, min_max_scale,
+    cap_3std, cap_perc
 )
 
 
@@ -75,6 +75,8 @@ def test_mean_impute(epc_input):
     assert_series_equal(actual, expected, check_dtype=False)
 
 
+
+
 class TestModeImpute(object):
 
     def test_mode(self, epc_input):
@@ -104,3 +106,59 @@ class TestModeImpute(object):
         actual = mode_impute(df=epc_input, col='Facades', mode=10)['df']['Facades']
         expected = pd.Series([10, 2, 4, 2, 1, 10, 2, 3], name='Facades')
         assert_series_equal(actual, expected, check_dtype=False)
+
+class TestMedianImpute (object):
+
+    def test_median(self, epc_input):
+        median = median_impute(df=epc_input, col='Facades', median=None)['median']
+        assert median == 2
+    def test_impute_md(self, epc_input):
+        df_actual = median_impute(df=epc_input, col='Facades', median=None)['df']
+
+
+        df_expected = pd.DataFrame(
+            data=[
+                [3.25e+05, 3, 2, 'Hendrik De Braekeleerlaan 68', 2.59e+02, 0, 1],
+                [1.74e+05, 2, 2, 'Antwerpsesteenweg 50', 6.44e+02, 0, 1],
+                [1.74e+05, 2, 4, 'Antwerpsesteenweg 5', 6.44e+02, 0, 1],
+                [1.99e+05, 2, 2, 'Hoevelei 194', 2.06e+02, 0, 1],
+                [2.39e+05, 2, 1, 'Leon Gilliotlaan 40', 2.29e+02, 0, -1],
+                [2.85e+05, 3, 2, 'De Cranelei 9', 4.40e+01, 0, -1],
+                [3.69e+05, 1, 2, 'Kapellestraat 159', 0.00e+00, 0, -1],
+                [3.69e+05, 1, 3, 'Kapellestraat 159 B', 0.00e+00, 0, -1],
+            ],
+            columns=['Price', 'Bedrooms', 'Facades', 'Street', 'EPC', 'Zeros', 'Ones']
+        )
+
+        assert_frame_equal(df_actual, df_expected, check_dtype=False)
+
+    def test_impute_with_median(self, epc_input):
+        actual = median_impute(df=epc_input, col='Facades', median=7)['df']['Facades']
+        expected = pd.Series([7, 2, 4, 2, 1, 7, 2, 3], name='Facades')
+        assert_series_equal(actual, expected, check_dtype=False)
+
+
+class TestCapPerc(object):
+    def test_value_cap(self, epc_input):
+        df_actual = cap_perc(epc_input, col='Facades', cap=0.99)['df']
+
+        df_expected = pd.DataFrame(
+            data=[
+                [3.25e+05, 3, None, 'Hendrik De Braekeleerlaan 68', 2.59e+02, 0, 1],
+                [1.74e+05, 2, 2, 'Antwerpsesteenweg 50', 6.44e+02, 0, 1],
+                [1.74e+05, 2, 4, 'Antwerpsesteenweg 5', 6.44e+02, 0, 1],
+                [1.99e+05, 2, 2, 'Hoevelei 194', 2.06e+02, 0, 1],
+                [2.39e+05, 2, 1, 'Leon Gilliotlaan 40', 2.29e+02, 0, -1],
+                [2.85e+05, 3, None, 'De Cranelei 9',  4.40e+01, 0, -1],
+                [3.69e+05, 1, 2, 'Kapellestraat 159', 0.00e+00, 0, -1],
+                [3.69e+05, 1, 3, 'Kapellestraat 159 B', 0.00e+00, 0, -1],
+
+            ],
+            columns=['Price', 'Bedrooms', 'Facades', 'Street', 'EPC', 'Zeros', 'Ones']
+        )
+        assert_frame_equal(df_actual, df_expected, check_dtype=False)
+
+
+
+
+
