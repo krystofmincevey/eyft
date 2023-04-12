d806bfc0a2a2cf104d05c1909340dd9fcf01c0d4
ARED2000 <arthur.decaluwe@be.ey.com>
2023-04-12 17:52:34 +0200

    Pearson (not finished)

--- src/eyft/pipelines/feature_selection/nodes.py
+++ src/eyft/pipelines/feature_selection/nodes.py
@@ -212,6 +212,8 @@ def lasso(
     using lasso regressions to determine weights.
     """
 
+    lasso(df, cutoff=)
+
     # TODO: Arthur
     raise NotImplementedError
     # return features
@@ -219,9 +221,7 @@ def lasso(
 
 def pearson(
     df: pd.DataFrame,
-    col1: str,
-    col2: str,
-    cutoff: float = 0.8,
+    cutoff: float = 0.80,
 ) -> List[str]:
     """
     Keep features that have a pearson
@@ -233,17 +233,11 @@ def pearson(
 
     check correlations and only keep either 2x or x but not both!
     """
-
-    pearson = df[col1].corr(df[col2], method='pearson')
-
     corr_matrix = df.corr()
     upper = corr_matrix.where((np.triu(np.ones(corr_matrix.shape), k=1) +
                                np.tril(np.ones(corr_matrix.shape), k=-1)).astype(bool))
     to_drop = [column for column in upper.columns if any(upper[column] > 0.80)]
     df.drop(to_drop, axis=1, inplace=True)
-
-
-    # TODO: Arthur
 
     raise NotImplementedError
     # return features
